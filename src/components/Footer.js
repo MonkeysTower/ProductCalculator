591393e5ba0e16f3bf238c2227a872707783ab3c
MonkeysTower <TrifAndre@yandex.ru>
2024-12-12 15:58:52 +0500

    Add ScrollTop on support button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,11 +9,19 @@ const Footer = ({ onSupport }) => {
         <p>Email: Trifandre@yandex.ru</p>        
       </div>
       <div>
-        <button className="support-button" type="button" onClick={onSupport}>Техподдержка</button>
+        <button id="support-button" className="support-button" type="button" onClick={onSupport}>Техподдержка</button>
       </div>
       <div className="my-info">
         <p>Designed by <i><a href="https://vk.com/fyl94">Dostovalov Andrey</a></i></p>
       </div>
+      <script>
+        const button = document.getElementById('support-button');
+        button.addEventListener('click', () => {
+          window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+          })});
+      </script>
     </footer>
   );
 };
